e272a387ceb115160c5caef959f1baf91ddea31e
aalmiray <aalmiray@gmail.com>
2017-06-01 11:30:45 +0200

    Update to latest Griffon

--- subprojects/griffon-hibernate3-core/src/main/java/org/codehaus/griffon/runtime/hibernate3/DefaultHibernate3Factory.java
+++ subprojects/griffon-hibernate3-core/src/main/java/org/codehaus/griffon/runtime/hibernate3/DefaultHibernate3Factory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 the original author or authors.
+ * Copyright 2014-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,7 +66,6 @@ public class DefaultHibernate3Factory extends AbstractObjectFactory<SessionFacto
 
     @Inject
     private Metadata metadata;
-
 
     @Inject
     public DefaultHibernate3Factory(@Nonnull @Named("hibernate3") griffon.core.Configuration configuration, @Nonnull GriffonApplication application) {
